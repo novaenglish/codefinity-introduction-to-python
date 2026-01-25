4b50cee9ef886b720815aba8037eb1a918fe71c1
novaenglish <>
2026-01-25 20:49:55 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,14 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove("onions")
+print(vegetables)
+vegetables.append("carrots")
+print(vegetables)
+vegetables.append("cucumbers")
+vegetables.sort()
+print(vegetables)
+print(f"Updated Vegetable Inventory: {vegetables}")
+
+
+
+
+
