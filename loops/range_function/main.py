faf84b72aa2c97310feb7efe06c72a4eeb25be29
novaenglish <>
2026-02-04 20:54:13 +0000

    Range Function

---
+++ loops/range_function/main.py
@@ -0,0 +1,8 @@
+# List of products on promotion for each weekday
+daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
+
+# List of weekdays corresponding to the promotions
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+for day in range(5):
+    print(f"{weekdays[day]}: Promotion on {daily_promotions[day]}")
